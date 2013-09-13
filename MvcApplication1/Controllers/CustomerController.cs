a3e31d94e57ee0483b3bd31460e46375e33950ba
alexB <bondor29@gmail.com>
2013-09-13 16:57:27 +0300

    test

---
+++ MvcApplication1/Controllers/CustomerController.cs
@@ -0,0 +1,49 @@
+﻿using MvcApplication1.Models.UserModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details()
+        { 
+            /* create object of type customer */
+            /* lista de interese care vinde dintr un tabel din db ..*/
+             List<string> interese = new List<string>();
+             interese.Add("belit");
+             interese.Add("asfasdfas");
+             interese.Add("asfasdfas");
+             interese.Add("asfasdfas");
+             interese.Add("asfasdfas");
+             interese.Add("asfasdfas");
+             interese.Add("asfasdfas");
+
+            var customer = new UserModel();
+            customer.Name = "Alex";
+            customer.Email = "xxx@gmail.com";
+            customer.Age = 20;
+            customer.Interests = new List<string>();// asta ii hack sa ai lista initializata ca altfel adaugi elementre intr o lista care nu exista
+            //cand le aduci din db nu ai problema asta
+
+            // mai jos ar fi defapt, pt fiecare interes al userului din db, baga-l in lista, iar in view, pt firecare element
+            //din lista de interese, baga l in <ul>
+
+            foreach (var item in interese)
+            {
+                //if(item != null)
+                    customer.Interests.Add(item);
+            }
+    
+            return View(customer);
+        }
+    }
+}
---
+++ MvcApplication1/Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Models.UserModel
+{
+    public class UserModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public List<string> Interests { get; set; }
+
+    }
+}
